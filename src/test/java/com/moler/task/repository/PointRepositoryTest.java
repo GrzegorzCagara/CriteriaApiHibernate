5a41c2c9c44f013190390587f3e524baef7dc6d6
Moler <moler555@gmail.com>
2018-03-23 08:20:36 +0100

    test getAll Point repo

--- src/test/java/com/moler/task/repository/PointRepositoryTest.java
+++ src/test/java/com/moler/task/repository/PointRepositoryTest.java
@@ -14,6 +14,11 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -36,14 +41,36 @@ public class PointRepositoryTest {
 
     @Test
     public void testGetAll(){
+        List<Point> points = new ArrayList<>();
+        points.add(mock(Point.class));
+        points.add(mock(Point.class));
+        points.add(mock(Point.class));
+        points.add(mock(Point.class));
         when(em.getCriteriaBuilder()).thenReturn(cb);
         when(cb.createQuery(Point.class)).thenReturn(query);
         when(query.from(Point.class)).thenReturn(rootPoint);
         when(em.createQuery(query)).thenReturn(q);
-        
+        when(q.getResultList()).thenReturn(points);
+
+        List<Point> result = pointRepository.getAll();
+        int pointsSize = points.size();
+
+        assertNotNull(result);
+        assertEquals(result.size(), pointsSize);
         verify(em, times(1)).getCriteriaBuilder();
         verify(em, times(1)).createQuery(query);
         verify(query, times(1)).from(Point.class);
         verify(cb, times(1)).createQuery(Point.class);
     }
+
+    @Test
+    public void testSave(){
+        when(em.merge(point)).thenReturn(point);
+
+        Point result = pointRepository.save(point);
+
+        assertNotNull(result);
+        verify(em, times(1)).merge(point);
+    }
+    
 }
